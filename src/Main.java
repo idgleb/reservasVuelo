e68d758da38864aed4a56bc3ab4568e0b61047ab
idgleb2 <id_gleb@mail.ru>
2024-09-18 00:02:38 -0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Aerolinea aerolinea = new Aerolinea("Aeroflot");
-        
+
         Vuelo vuelo = new Vuelo("EZS123", aerolinea);
 
         aerolinea.reservar_asiento("Gleb", 12434, vuelo);
